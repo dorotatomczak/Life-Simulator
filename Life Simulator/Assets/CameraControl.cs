8cbf59648b6acc9eb05899286e63c865301c1b08
Dorota <tomczakd65@gmail.com>
2018-04-07 00:18:40 +0200

    Added right click camera movement

---
+++ Life Simulator/Assets/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public float dragSpeed = 2;
+	private Vector3 dragOrigin;
+
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+			dragOrigin = Input.mousePosition;
+			return;
+		}
+
+		if (!Input.GetMouseButton(1)) return;
+
+		Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+		Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+		transform.Translate(move, Space.World);
+	}
+
+}
