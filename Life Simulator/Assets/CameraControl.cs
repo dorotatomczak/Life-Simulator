6454942aba91783175dc35946fd2ab42ae10e5de
Dorota <tomczakd65@gmail.com>
2018-04-15 16:36:18 +0200

    New skybox, status bars

--- Life Simulator/Assets/CameraControl.cs
+++ Life Simulator/Assets/CameraControl.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
-	protected Vector3 localRotation;
-	public float mouseSensitivity = 4.0f;
-	public float orbitDampening = 10.0f;
-	public float rotateMin = -45.0f;
-	public float rotateMax = 45.0f;
+	[System.Serializable]
+	public class RotationSettings
+	{
+		public float mouseSensitivity = 4.0f;
+		public float orbitDampening = 10.0f;
+		public float rotateMin = -45.0f;
+		public float rotateMax = 45.0f;
+	}
 
-	public float zoomSensitivity= 15.0f;
-	public float zoomSpeed= 5.0f;
-	public float zoomMin= 5.0f;
-	public float zoomMax= 80.0f;
-	private float zoom;
+	[System.Serializable]
+	public class ZoomSettings
+	{
+		public float zoomSensitivity= 15.0f;
+		public float zoomSpeed= 5.0f;
+		public float zoomMin= 5.0f;
+		public float zoomMax= 80.0f;
+	}
 
-	public float dragSpeed = 0.2f;
-	private Vector3 dragOrigin;
+	[System.Serializable]
+	public class DragSettings
+	{
+		public float dragSpeed = 0.2f;
+	}
+
+	Vector3 localRotation;
+	float zoom;
+	Vector3 dragOrigin;
+
+	public RotationSettings rotationSettings = new RotationSettings ();
+	public ZoomSettings zoomSettings = new ZoomSettings ();
+	public DragSettings dragSetttings =  new DragSettings ();
 
 	void Start()
 	{
@@ -26,25 +43,25 @@ public class CameraControl : MonoBehaviour {
 
 	void Update() 
 	{
-		zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
-		zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+		zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSettings.zoomSensitivity;
+		zoom = Mathf.Clamp(zoom, zoomSettings.zoomMin, zoomSettings.zoomMax);
 	}
 
 	void LateUpdate()
 	{
 		//zooming
-		Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, zoom, Time.deltaTime * zoomSpeed);
+		Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, zoom, Time.deltaTime * zoomSettings.zoomSpeed);
 
 		//rotating
 		if (Input.GetMouseButton (2) && (Input.GetAxis ("Mouse X") != 0 || Input.GetAxis ("Mouse Y") != 0)) 
 		{
-			localRotation.x += Input.GetAxis ("Mouse X") * mouseSensitivity;
-			localRotation.y -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+			localRotation.x += Input.GetAxis ("Mouse X") * rotationSettings.mouseSensitivity;
+			localRotation.y -= Input.GetAxis ("Mouse Y") * rotationSettings.mouseSensitivity;
 
-			localRotation.y = Mathf.Clamp (localRotation.y, -45f, 45f);
+			localRotation.y = Mathf.Clamp (localRotation.y, rotationSettings.rotateMin, rotationSettings.rotateMax);
 
 			Quaternion QT = Quaternion.Euler (localRotation.y, localRotation.x, 0);
-			transform.rotation = Quaternion.Lerp (transform.rotation, QT, Time.deltaTime * orbitDampening);
+			transform.rotation = Quaternion.Lerp (transform.rotation, QT, Time.deltaTime * rotationSettings.orbitDampening);
 		}
 
 		//click and drag
@@ -57,7 +74,7 @@ public class CameraControl : MonoBehaviour {
 		if (!Input.GetMouseButton(1)) return;
 
 		Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
-		Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+		Vector3 move = new Vector3(pos.x * dragSetttings.dragSpeed, 0, pos.y * dragSetttings.dragSpeed);
 
 		transform.Translate(move, Space.World);
 	}
---
+++ Life Simulator/Assets/StatsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsController : MonoBehaviour {
+
+	[SerializeField]
+	Stat hunger;
+
+	[SerializeField]
+	Stat bladder;
+
+	[SerializeField]
+	Stat energy;
+
+	[SerializeField]
+	Stat social;
+
+	[SerializeField]
+	Stat hygiene;
+
+	[SerializeField]
+	Stat fun;
+
+	void Awake () {
+		hunger.Initialize ();
+		bladder.Initialize ();
+		energy.Initialize ();
+		social.Initialize ();
+		hygiene.Initialize ();
+		fun.Initialize ();
+	}
+
+	void Update () {
+		hunger.CurrentVal -= hunger.Rate * Time.deltaTime;
+		bladder.CurrentVal -= bladder.Rate * Time.deltaTime;
+		energy.CurrentVal -= energy.Rate * Time.deltaTime;
+		social.CurrentVal -= social.Rate * Time.deltaTime;
+		hygiene.CurrentVal -= hygiene.Rate * Time.deltaTime;
+		fun.CurrentVal -= fun.Rate * Time.deltaTime;
+	}
+}
