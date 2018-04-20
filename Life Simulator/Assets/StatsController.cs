ae29b7cb256286a7494d24836c4b70f4b21fabb8
magasol <magdalena.j.solecka@gmail.com>
2018-04-20 13:05:11 +0200

    Added interaction with refrigerator and bed, without time required to walk near them.

--- Life Simulator/Assets/StatsController.cs
+++ Life Simulator/Assets/StatsController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class StatsController : MonoBehaviour {
 
-	[SerializeField]
+    public static bool refillingEnergyBar;
+    public static bool refillingHungerBar;
+
+    [SerializeField]
 	Stat hunger;
 
 	[SerializeField]
@@ -29,14 +32,42 @@ public class StatsController : MonoBehaviour {
 		social.Initialize ();
 		hygiene.Initialize ();
 		fun.Initialize ();
-	}
+        refillingEnergyBar = false;
+        refillingHungerBar = false;
+    }
 
 	void Update () {
-		hunger.CurrentVal -= hunger.Rate * Time.deltaTime;
+        if (!refillingHungerBar)
+            hunger.CurrentVal -= hunger.Rate * Time.deltaTime;
+        else {
+            hunger.CurrentVal += 6.0f * Time.deltaTime;
+            if (hunger.CurrentVal >= 100)
+            {
+                hunger.CurrentVal = 100;
+                refillingHungerBar = false;
+            }
+        }
+
 		bladder.CurrentVal -= bladder.Rate * Time.deltaTime;
-		energy.CurrentVal -= energy.Rate * Time.deltaTime;
+
+        if(!refillingEnergyBar)
+		    energy.CurrentVal -= energy.Rate * Time.deltaTime;
+        else {
+            energy.CurrentVal += 3.0f * Time.deltaTime;
+            if(energy.CurrentVal >= 100) {
+                energy.CurrentVal = 100;
+                refillingEnergyBar = false;
+            }
+        }
 		social.CurrentVal -= social.Rate * Time.deltaTime;
 		hygiene.CurrentVal -= hygiene.Rate * Time.deltaTime;
 		fun.CurrentVal -= fun.Rate * Time.deltaTime;
 	}
+    public static void RefillEnergyBar() {
+        refillingEnergyBar = true;
+    }
+    public static void RefillHungerBar()
+    {
+        refillingHungerBar = true;
+    }
 }
